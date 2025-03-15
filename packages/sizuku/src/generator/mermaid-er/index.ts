8fc2c63365f762402b64db214645cb5ae0cacc5a
nakita628 <nakita@ypm.llc>
2025-03-15 04:34:50 +0000

    generate mermaid-er

---
+++ packages/sizuku/src/generator/mermaid-er/index.ts
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+import type { Config } from './config'
+import type { RelationType } from './type'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import { generateERContent } from './generator/generate-er-content'
+import { parseTableInfo } from './validator/parse-table-info'
+import { extractRelations } from './core/extract-relations'
+import { getConfig } from './config'
+import { argv } from 'node:process'
+
+export const CARDINALITY_MAP: Record<RelationType, string> = {
+  // both required
+  'one-to-one': '||--||', // 1--1
+  'one-to-many': '||--o{', // 1--*
+  'many-to-one': '}o--||', // *--1
+  'many-to-many': '}o--o{', // *--*
+  // one side optional
+  'one-to-zero-one': '||--o|', // 1--0..1
+  'zero-one-to-one': 'o|--||', // 0..1--1
+  'zero-to-many': 'o|--o{', // 0--*
+  'many-to-zero-one': '}o--o|', // *--0..1
+  'zero-one-to-many': 'o|--o{', // 0..1--*
+  // o o
+  'zero-to-one': 'o|--o|', // 0--0..1
+  'zero-to-zero-one': 'o|--o|', // 0--0..1
+  // ..
+  'one-to-many-optional': '||..o{', // 1..*
+  'one-to-one-optional': '||..||', // 1..1
+  'many-to-many-optional': '}o..o{', // *..*
+  'zero-one-to-zero-one': 'o|..o|', // 0..1--0..1
+  'many-to-zero-many': '}o..o{', // *--0..*
+  'zero-many-to-many': 'o{..}o', // 0..*--*
+  'zero-many-to-zero-many': 'o{..o{', // 0..*--0..*
+}
+
+// ER diagram header
+export const ER_HEADER = ['```mermaid', 'erDiagram'] as const
+
+// ER diagram footer
+export const ER_FOOTER = ['```'] as const
+
+export async function main(dev = false, config: Config = getConfig()) {
+  // 1. argv ['**/bin/node', '**/dist/src/generator/mermaid-er/index.js', 'db/schema.ts', '-o', 'mermaid-er/ER.md']
+  if (config.output === undefined && !argv.includes('-o')) {
+    console.error('Error: -o is not found')
+    return false
+  }
+  // 2. slice [ 'db/schema.ts', '-o', 'mermaid-er/ER.md']
+  const args = process.argv.slice(2)
+  // 3. input = args[0] = 'db/schema.ts'
+  const input = config.input ?? args[0]
+  config.input = input
+  // 4. output = 'mermaid-er/ER.md'
+  const output = config.output ?? args[args.indexOf('-o') + 1]
+  config.output = output
+
+  try {
+    // 5. read db/schema.ts
+    const content = readFileSync(input, 'utf-8')
+    // 6. split lines
+    const lines = content.split('\n')
+    // 7. create output directory
+    const outputDir = path.dirname(output)
+    if (!existsSync(outputDir)) {
+      mkdirSync(outputDir, { recursive: true })
+    }
+
+    // 8. skip import section
+    const codeStart = lines.findIndex(
+      (line) => !line.trim().startsWith('import') && line.trim() !== '',
+    )
+    // 9. parse table info
+    const tables = parseTableInfo(lines.slice(codeStart))
+
+    // 10. extract relations
+    const relations = extractRelations(lines.slice(codeStart))
+    // 11. generate ER content
+    const ERContent = generateERContent(relations, tables)
+    // 12. write ER content to output file
+    writeFileSync(output, ERContent)
+    console.log(`Generated ER at: ${output}`)
+    return true
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message)
+      if (dev) {
+        throw e
+      }
+      process.exit(1)
+    }
+    if (dev) {
+      throw new Error('Unknown error occurred')
+    }
+    return false
+  }
+}
+
+if (require.main === module) {
+  main().then((success) => {
+    if (!success) {
+      process.exit(1)
+    }
+  })
+}
