072f55d21e57b3624472e472f20109836cbcc787
nakita628 <nakita@ypm.llc>
2025-06-29 10:17:44 +0000

    change setting

--- packages/sizuku/vitest.config.ts
+++ packages/sizuku/vitest.config.ts
@@ -1,30 +1,10 @@
 import { defineConfig } from 'vitest/config'
-
 export default defineConfig({
   test: {
-    exclude: [
-      '**/dist/**',
-      '**/src/data/*.ts',
-      '**/src/type/*.ts',
-      '**/node_modules/**',
-      '**/db/schema.ts',
-      '**/common/config/index.ts',
-    ],
+    includeSource: ['src/**/*.ts'],
+    include: ['src/**/*.test.ts'],
     coverage: {
-      exclude: [
-        '**/src/**/*.test.ts',
-        '**/dist/**',
-        '**/type/*.ts',
-        '**/node_modules/**',
-        '**/vitest.config.ts',
-        '**/common/config/index.ts',
-        '**/db/schema.ts',
-        '**/generator/mermaid-er/config/*.ts',
-        '**/generator/valibot/config/*.ts',
-        '**/generator/zod/config/*.ts',
-        './valibot/*.ts',
-        './zod/*.ts',
-      ],
+      include: ['src/**/*.ts'],
       all: true,
     },
   },
