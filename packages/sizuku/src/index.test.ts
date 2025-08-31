04b8f1515173521cc4ddeb1e6b9560a2211bf6e2
nakita628 <nakita@ypm.llc>
2025-08-31 09:58:46 +0000

    add

--- packages/sizuku/src/index.test.ts
+++ packages/sizuku/src/index.test.ts
@@ -43,7 +43,8 @@ export const user = mysqlTable('user', {
   it('main success', async () => {
     // Create config file
     const configFile = path.join(tmpdir, 'sizuku.config.ts')
-    const configContent = `
+    const configContent = `import { defineConfig } from './src/config/index.js'
+
 export default defineConfig({
   input: 'db/schema.ts',
   zod: {
